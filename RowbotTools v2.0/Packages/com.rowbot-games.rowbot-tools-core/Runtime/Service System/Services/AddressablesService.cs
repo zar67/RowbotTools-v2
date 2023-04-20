b203a2cdff3ca24b46d702e7f0ca1b3b01b52934
Zoe <zoerowbotham1@gmail.com>
2023-04-20 01:51:59 +0100

    Adding addressables service

---
+++ RowbotTools v2.0/Packages/com.rowbot-games.rowbot-tools-core/Runtime/Service System/Services/AddressablesService.cs
@@ -0,0 +1,72 @@
+namespace RowbotTools.Core.ServiceSystem
+{
+    using System;
+    using UnityEngine;
+    using UnityEngine.AddressableAssets;
+    using UnityEngine.AddressableAssets.ResourceLocators;
+    using UnityEngine.ResourceManagement.AsyncOperations;
+
+    /// <summary>
+    /// A service for managing the loading and unloading of addressables.
+    /// </summary>
+    public class AddressablesService : Service
+    {
+        private bool m_isInitialized = false;
+
+        /// <summary>
+        /// Initialization of the addressables.
+        /// </summary>
+        public override void Init()
+        {
+            base.Init();
+
+            m_isInitialized = false;
+
+            Addressables.InitializeAsync().Completed += OnAddressablesInitialized;
+        }
+
+        /// <summary>
+        /// Cleanup of the addressables.
+        /// </summary>
+        public override void Cleanup()
+        {
+            base.Cleanup();
+        }
+
+        /// <summary>
+        /// Loads an asset of a given reference. Calls the loadedCallback if asset is successfully loaded.
+        /// </summary>
+        public void LoadAsset<T>(AssetReference assetReference, Action<T> loadedCallback)
+        {
+            if (!m_isInitialized)
+            {
+                Debug.LogWarning("Addressables aren't properly initialized, please make sure addressables are initialized before loading assets.");
+                return;
+            }
+
+            assetReference.LoadAssetAsync<T>().Completed += (asyncOperationHandle) =>
+            {
+                if (asyncOperationHandle.Status == AsyncOperationStatus.Succeeded)
+                {
+                    loadedCallback?.Invoke(asyncOperationHandle.Result);
+                }
+                else
+                {
+                    Debug.LogError($"Failed to load adressable {assetReference.Asset.name}");
+                }
+            };
+        }
+
+        private void OnAddressablesInitialized(AsyncOperationHandle<IResourceLocator> asyncOperationHandle)
+        {
+            if (asyncOperationHandle.Status == AsyncOperationStatus.Succeeded)
+            {
+                m_isInitialized = true;
+            }
+            else
+            {
+                Debug.LogError("Failed to initialize addressables");
+            }
+        }
+    }
+}
